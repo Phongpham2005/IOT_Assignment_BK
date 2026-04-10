daf798196298bb93d821e5d1fd93236a07d251a3
Phongpham2005 <phongphamsym@gmail.com>
2026-04-10 14:26:12 +0700

    commit Task 1 2 3

--- include/global.h
+++ include/global.h
@@ -6,8 +6,19 @@
 #include "freertos/task.h"
 #include "freertos/semphr.h"
 
-extern float glob_temperature;
-extern float glob_humidity;
+// extern float glob_temperature;
+// extern float glob_humidity;
+
+typedef struct {
+    float temperature;
+    float humidity;
+    // Mutex to protect concurrent read/write access to temp/humidity
+    SemaphoreHandle_t dataMutex;         
+    // Binary Semaphores for Task Synchronization
+    SemaphoreHandle_t ledSemaphore;      // Triggered on Temperature changes
+    SemaphoreHandle_t neoSemaphore;      // Triggered on Humidity changes
+    SemaphoreHandle_t lcdSemaphore;      // Triggered on Display State changes
+} SystemData_t;
 
 extern String WIFI_SSID;
 extern String WIFI_PASS;
